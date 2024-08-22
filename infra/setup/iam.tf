7e2adaee72fac14f2edeb110a9d1ff21d8069cb4
Mohamed Shire <shirem33@gmail.com>
2024-08-23 00:50:58 +0100

    Added iam user for cicd

---
+++ infra/setup/iam.tf
@@ -0,0 +1,79 @@
+###############################################################
+# Create IAM user and policies for Continuous CD account (CD) #
+###############################################################
+
+resource "aws_iam_user" "cd" {
+  name = "recipe-app-api-cd"
+}
+# We create a CD user for github actions to be able to use AWS
+
+resource "aws_iam_access_key" "cd" {
+  user = aws_iam_user.cd.name
+}
+# Access key that acts as a password for machine to machine 
+# Authentification
+
+###########################################################
+# Policy for Terraform backkend to S3 and Dynamo DB access#
+###########################################################
+
+data "aws_iam_policy_document" "tf_backend" {
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::${var.tf_state_bucket}"]
+  }
+  # Allow list bucket permission on this bucket
+
+  statement {
+    effect  = "Allow"
+    actions = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+    resources = [
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy/*",
+      "arn:aws:s3:::${var.tf_state_bucket}/tf-state-deploy-env/*"
+    ]
+  }
+  # Allow to get, put, detete object into into those parts of the bucket
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:DescribeTable",
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = ["arn:aws:dynamodb:*:*:table/${var.tf_state_lock_table}"]
+  }
+}
+# Allow describe, get, put and delete item on the dynamodb table
+
+resource "aws_iam_policy" "tf_backend" {
+  name        = "${aws_iam_user.cd.name}-tf-s3-dynamodb"
+  description = "Allow user to use S3 and DynamoDB for TF backend resources"
+  policy      = data.aws_iam_policy_document.tf_backend.json
+}
+# Use the policy document for this iam policy
+
+resource "aws_iam_user_policy_attachment" "tf_backend" {
+  user       = aws_iam_user.cd.name
+  policy_arn = aws_iam_policy.tf_backend.arn
+}
+# Give this policy to the CD user
+
+#########################
+# Policy for ECR access #
+#########################
+
+
+
+#########################
+# Policy for EC2 access #
+#########################
+
+
+
+
+#########################
+# Policy for RDS access #
+#########################
