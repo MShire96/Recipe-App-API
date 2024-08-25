5554eeef288ead58e5e5b7705fcd9b89a757bf0f
Mohamed Shire <shirem33@gmail.com>
2024-08-25 21:06:34 +0100

    added task definition

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -38,15 +38,36 @@ resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
   policy_arn = aws_iam_policy.task_ssm_policy.arn
 }
 
-resource "aws_ecs_cluster" "main" {
-  name = "${local.prefix}-cluster"
-}
-
 resource "aws_cloudwatch_log_group" "ecs_task_logs" {
   name = "${local.prefix}-api"
+}
+resource "aws_ecs_cluster" "main" {
+  name = "${local.prefix}-cluster"
 }
 
 # All we need to create a new cluster in AWS
 # The cluster we'll be adding our services and our tasks to
 # It'll be the wrapper around the other resources.
 
+resource "aws_ecs_task_definition" "api" {
+  family = "${local.prefix}-api" # Essentially a name
+  # Each task definition, once it updates, creates a new version
+  # That new version comes under the family task of that definition
+  requires_compatibilities = ["FARGATE"]                          # We're using fargate, serverless
+  network_mode             = "awsvpc"                             # The type of network that will be used in our task
+  cpu                      = 256                                  # Cpu, the lowest cpu
+  memory                   = 512                                  # The lowest memory you can put
+  execution_role_arn       = aws_iam_role.task_execution_role.arn # ECS allowed to pull images
+  task_role_arn            = aws_iam_role.app_task.arn            # role assigned to task, permission for ssm manager
+
+  container_definitions = jsonencode([]) # Definition of containers that are going to the task
+
+  volume {
+    name = "static" # The volume available to our ECS task definition, docker volume
+  }
+
+  runtime_platform { # The type of server our containers are going to run on 
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+}
