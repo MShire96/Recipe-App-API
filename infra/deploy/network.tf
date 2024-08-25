1084ce95fb9d3958047381d3410d0aff623f8534
Mohamed Shire <shirem33@gmail.com>
2024-08-25 14:29:37 +0100

    added private networks

--- infra/deploy/network.tf
+++ infra/deploy/network.tf
@@ -125,3 +125,29 @@ resource "aws_route" "public_internet_access_b" {
 }
 # Create AWS route, connected to route table in public b
 # Access through internet gateway, destination to all IP addresses
+
+############################################
+# Private subnets for internal access only #
+############################################
+
+# Don't need to hook it up with internet gateway
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.10.0/24"
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = {
+    Name = "${local.prefix}-private-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.1.11.0/24"
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = {
+    Name = "${local.prefix}-private-b"
+  }
+}
