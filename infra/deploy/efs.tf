dccd7cd42d9fc035a211343805139a69f0a7785c
Mohamed Shire <shirem33@gmail.com>
2024-08-26 22:44:28 +0100

    added efs access point

--- infra/deploy/efs.tf
+++ infra/deploy/efs.tf
@@ -35,3 +35,16 @@ resource "aws_efs_mount_target" "media_b" {
   subnet_id       = aws_subnet.public_b.id
   security_groups = [aws_security_group.efs.id]
 }
+
+resource "aws_efs_access_point" "media" {       # Its a way you can split up the locations inside your EFS
+  file_system_id = aws_efs_file_system.media.id # And give different access to different things
+  root_directory {                              # We're creating one access as we only need one as we're going to store files for our media
+    path = "/api/media"                         # All in that location
+
+    creation_info {     # For when the file system are created, need to specify yhe permissions of who can access that in term of linux user ID
+      owner_gid   = 101 # Our user, defined in docker with UID 101, to have these owner permissions
+      owner_uid   = 101
+      permissions = "755" # chmod permissions 
+    }
+  }
+}
